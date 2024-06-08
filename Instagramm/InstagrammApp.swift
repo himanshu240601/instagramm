5c42bf0459a64bfbe1bfe1f8ed4f08060212d14c
Himanshu Goyal <goyahim69@gmail.com>
2024-06-08 19:56:42 +0530

    Initial Commit

---
+++ Instagramm/InstagrammApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagrammApp.swift
+//  Instagramm
+//
+//  Created by Himanshu's MacBook on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagrammApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
